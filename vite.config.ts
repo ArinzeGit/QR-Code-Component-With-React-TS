8183e0182228963ac235f6e65a0ed4df2e727100
Arinze Owoh <arinzeowoh@gmail.com>
2024-04-04 04:01:09 +0100

    config edit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/qr-code-component-with-react-ts/"
+  base:"/QR-Code-Component-With-React-TS/"
 })
